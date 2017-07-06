1be7ad3a601260d9585b02c76929241c8d140d96
mvanmaele <mvanmaele@talusanalytics.com>
2017-07-06 18:22:12 -0400

    add index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// NodeJS script for starting server and listening for HTTP requests
+var app = require('express')();
+var server = require('http').Server(app);
+var path = require('path');
+var axios = require('axios');
+
+// Routing
+// if no hash, send to index
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+
+// if hash, send to requested resource
+app.get(/^(.+)$/, function(req, res) {
+	res.sendFile(path.join(__dirname, '/', req.params[0]));
+});	
+
+// Start the HTTP Server
+server.listen(process.env.PORT || 8888, function() {
+	console.log('Server set up!');
+	console.log(server.address());
+});
