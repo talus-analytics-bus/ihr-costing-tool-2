4326371c30ef579b6bf4500c666e73cbe81382f2
mvanmaele <mvanmaele@talusanalytics.com>
2017-07-10 10:31:35 -0400

    rmv comments

--- js/components/map.js
+++ js/components/map.js
@@ -1,14 +1,11 @@
 (() => {
   App.createCountryMap = () => {
-    // d3.select(window).on("resize", throttle);
-
     var zoom = d3.zoom()
         .scaleExtent([1, 9])
         .on("zoom", move);
 
     const mapContainer = d3.select('.map-container');
 
-    // var width = mapContainer.offsetWidth;
     let width = 700;
     var height = width / 2;
     var topo,projection,path,svg,g;
@@ -17,7 +14,6 @@
     setup(width,height);
 
     function setup(width,height){
-      //projection = d3.geoEquirectangular()
       projection = d3.geoMercator()
         .translate([(width/2), (height/2)])
         .scale( width / 2 / Math.PI)
@@ -63,10 +59,9 @@
     function draw(topo) {
 
       svg.append("path")
-         .datum(graticule)
-         .attr("class", "graticule")
-         .attr("d", path);
-
+       .datum(graticule)
+       .attr("class", "graticule")
+       .attr("d", path);
 
       g.append("path")
        .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
@@ -76,13 +71,12 @@
 
       var country = g.selectAll(".country").data(topo);
 
+      // add country shapes
       country.enter().insert("path")
           .attr("class", "country")
           .attr("d", path)
           .attr("id", function(d,i) { return d.id; })
           .attr("title", function(d,i) { return d.properties.name; });
-          // .style("fill", function(d, i) { return '#F0F0F0'; });
-          // .style("fill", function(d, i) { return d.properties.color; });
 
       //offsets for tooltips
       var offsetL = mapContainer.offsetLeft+20;
@@ -106,7 +100,6 @@
 
 
     function redraw() {
-      // width = width;
       height = width / 2;
       d3.select('svg').remove();
       setup(width, height);
@@ -116,9 +109,7 @@
 
     function move() {
 
-      //var t = d3.event.translate;
       var t = [d3.event.transform.x,d3.event.transform.y];
-      //var s = d3.event.scale; 
       var s = d3.event.transform.k;
       zscale = s;
       var h = height/4;
@@ -133,22 +124,12 @@
         Math.max(height  * (1 - s) - h * s, t[1])
       );
 
-      //zoom.translateBy(t);
       g.attr("transform", "translate(" + t + ")scale(" + s + ")");
 
       //adjust the country hover stroke width based on zoom level
       d3.selectAll(".country").style("stroke-width", 1.5 / s);
 
     }
-
-    // var throttleTimer;
-    // function throttle() {
-    //   window.clearTimeout(throttleTimer);
-    //     throttleTimer = window.setTimeout(function() {
-    //       redraw();
-    //     }, 200);
-    // }
-
 
     //geo translation on mouse click in map
     function click() {
