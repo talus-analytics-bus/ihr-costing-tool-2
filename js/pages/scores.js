1a9c99f65d3c654934a85e46636529c85bc7d367
mvanmaele <mvanmaele@talusanalytics.com>
2017-07-12 17:11:32 -0400

    add skeleton of scores page demo content

--- js/pages/scores.js
+++ js/pages/scores.js
@@ -60,7 +60,7 @@
 
 		// TODO add input blocks for each core capacity
 		addCoreCapacityTabs = () => {
-			d3.select('.block-container.input-block-container').selectAll('block')
+			const block = d3.select('.block-container.input-block-container').selectAll('block')
 				.data(blocksShowing)
 				.enter().append('div')
 					.attr('class', (d) =>  {
@@ -69,9 +69,32 @@
 		};
 		addCoreCapacityTabs();
 		
+		// style the scores page by adding gradient definition
+		styleScores = () => {
+			const slotGradient = d3.select('body').append('linearGradient')
+				.attr('class','indicator-slot-gradient')
+				.attr('x1',322.44)
+				.attr('y1',51.62)
+				.attr('x2',322.44)
+				.attr('gradientUnits','userSpaceOnUse');
+
+			slotGradient.append('stop')
+				.attr('offset','0')
+				.attr('stop-color','#e6e7e8');
+				
+			slotGradient.append('stop')
+				.attr('offset','1')
+				.attr('stop-color','#fff');
+		};
+		styleScores();
+		
 		// call function to render the tabs
 		App.setupTabs(blocksShowing, blocks);
 
+		// DEMO show the fake-block html in the AMR example
+		// TODO setup the block content dynamically
+		const demoScoringHtml = $('.fake-block').html();
+		$('.p3-block').html(demoScoringHtml);
 
 	};
 })();
