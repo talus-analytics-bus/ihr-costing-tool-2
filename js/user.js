fb564737029ad90c774a424d01221bfe4673c340
mvanmaele <mvanmaele@talusanalytics.com>
2017-07-13 10:17:57 -0400

    add user.js

---
+++ js/user.js
@@ -0,0 +1,13 @@
+const User = {};
+(() => {
+	// TODO fill in user data
+	User.scores = {
+		'P.3.1': {
+			score: null, // set on scores page
+			step: null // default: plus one
+		}
+	};
+
+	// DEMO set scores for AMR indicators
+
+})();
