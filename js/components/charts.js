4e34ab3fcd48c79286210f32683c8d0a6cec580b
mvanmaele <mvanmaele@talusanalytics.com>
2017-08-29 12:40:48 -0400

    add black rings on buibble hover

--- js/components/charts.js
+++ js/components/charts.js
@@ -249,6 +249,7 @@ const Charts = {};
 			.attr('width', width + margin.left + margin.right)
 			.attr('height', height + margin.top + margin.bottom);
 		const chart = chartContainer.append('g')
+			.attr('class','bubble-pack')
 			.attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 		// setup data
@@ -336,6 +337,7 @@ const Charts = {};
 		node.append('circle')
 			.attr('id', d => d.id)
 			.attr('r', 0)
+			.attr('class','bubble')
 			.style('fill', d => scaleColor(d.cat))
 			.transition().duration(2000).ease(d3.easeElasticOut)
 				.tween('circleIn', (d) => {
@@ -351,153 +353,22 @@ const Charts = {};
 			.append('use')
 			.attr('xlink:href', d => `#${d.id}`);
 
-		// // display text as circle icon
-		// node.filter(d => !String(d.icon).includes('img/'))
-		// 	.append('text')
-		// 	.classed('node-icon', true)
-		// 	.attr('clip-path', d => `url(#clip-${d.id})`)
-		// 	.selectAll('tspan')
-		// 	.data(d => d.icon.split(';'))
-		// 	.enter()
-		// 		.append('tspan')
-		// 		.attr('x', 0)
-		// 		.attr('y', (d, i, nodes) => (13 + (i - nodes.length / 2 - 0.5) * 10))
-		// 		.text(name => name);
-
-		// // display image as circle icon
-		// node.filter(d => String(d.icon).includes('img/'))
-		// 	.append('image')
-		// 	.classed('node-icon', true)
-		// 	.attr('clip-path', d => `url(#clip-${d.id})`)
-		// 	.attr('xlink:href', d => d.icon)
-		// 	.attr('x', d => -d.radius * 0.7)
-		// 	.attr('y', d => -d.radius * 0.7)
-		// 	.attr('height', d => d.radius * 2 * 0.7)
-		// 	.attr('width', d => d.radius * 2 * 0.7);
-
 		node.append('title')
 			.text(d => (d.name + '\n' + format(d.value)));
-		// node.append('title')
-		// 	.text(d => (d.cat + '::' + d.name + '\n' + format(d.value)));
 
-		// let infoBox = node.append('foreignObject')
-		// 	.classed('circle-overlay hidden', true)
-		// 	.attr('x', -350 * 0.5 * 0.8)
-		// 	.attr('y', -350 * 0.5 * 0.8)
-		// 	.attr('height', 350 * 0.8)
-		// 	.attr('width', 350 * 0.8)
-		// 		.append('xhtml:div')
-		// 		.classed('circle-overlay__inner', true);
-
-		// infoBox.append('h2')
-		// 	.classed('circle-overlay__title', true)
-		// 	.text(d => d.name);
-
-		// infoBox.append('p')
-		// 	.classed('circle-overlay__body', true)
-		// 	.html(d => d.desc);
-
-
-		node.on('click', (currentNode) => {
-
-			console.log('Clicked ' + currentNode.name + ', total = ' + currentNode.value);
-
-			// d3.event.stopPropagation();
-			// console.log('currentNode', currentNode);
-			// let currentTarget = d3.event.currentTarget; // the <g> el
-
-			// if (currentNode === focusedNode) {
-			// 	// no focusedNode or same focused node is clicked
-			// 	return;
-			// }
-			// let lastNode = focusedNode;
-			// focusedNode = currentNode;
-
-			// simulation.alphaTarget(0.2).restart();
-			// // hide all circle-overlay
-			// d3.selectAll('.circle-overlay').classed('hidden', true);
-			// d3.selectAll('.node-icon').classed('node-icon--faded', false);
-
-			// // don't fix last node to center anymore
-			// if (lastNode) {
-			// 	lastNode.fx = null;
-			// 	lastNode.fy = null;
-			// 	node.filter((d, i) => i === lastNode.index)
-			// 		.transition().duration(2000).ease(d3.easePolyOut)
-			// 		.tween('circleOut', () => {
-			// 			let irl = d3.interpolateNumber(lastNode.r, lastNode.radius);
-			// 			return (t) => {
-			// 				lastNode.r = irl(t);
-			// 			};
-			// 		})
-			// 		.on('interrupt', () => {
-			// 			lastNode.r = lastNode.radius;
-			// 		});
-			// }
-
-			// // if (!d3.event.active) simulation.alphaTarget(0.5).restart();
-
-			// d3.transition().duration(2000).ease(d3.easePolyOut)
-			// 	.tween('moveIn', () => {
-			// 		console.log('tweenMoveIn', currentNode);
-			// 		let ix = d3.interpolateNumber(currentNode.x, centerX);
-			// 		let iy = d3.interpolateNumber(currentNode.y, centerY);
-			// 		let ir = d3.interpolateNumber(currentNode.r, centerY * 0.5);
-			// 		return function (t) {
-			// 			// console.log('i', ix(t), iy(t));
-			// 			currentNode.fx = ix(t);
-			// 			currentNode.fy = iy(t);
-			// 			currentNode.r = ir(t);
-			// 			simulation.force('collide', forceCollide);
-			// 		};
-			// 	})
-			// 	.on('end', () => {
-			// 		simulation.alphaTarget(0);
-			// 		let $currentGroup = d3.select(currentTarget);
-			// 		$currentGroup.select('.circle-overlay')
-			// 			.classed('hidden', false);
-			// 		$currentGroup.select('.node-icon')
-			// 			.classed('node-icon--faded', true);
-
-			// 	})
-			// 	.on('interrupt', () => {
-			// 		console.log('move interrupt', currentNode);
-			// 		currentNode.fx = null;
-			// 		currentNode.fy = null;
-			// 		simulation.alphaTarget(0);
-			// 	});
-
+		node.on('mouseover', function(currentNode) {
+			// black ring
+			const circle = d3.select(this);
+			circle
+				.attr('stroke-width','3px')
+				.attr('stroke','black');
 		});
 
-		// blur
-		d3.select(document).on('click', () => {
-			let target = d3.event.target;
-			// check if click on document but not on the circle overlay
-			if (!target.closest('#circle-overlay') && focusedNode) {
-				focusedNode.fx = null;
-				focusedNode.fy = null;
-				simulation.alphaTarget(0.2).restart();
-				d3.transition().duration(2000).ease(d3.easePolyOut)
-					.tween('moveOut', function () {
-						console.log('tweenMoveOut', focusedNode);
-						let ir = d3.interpolateNumber(focusedNode.r, focusedNode.radius);
-						return function (t) {
-							focusedNode.r = ir(t);
-							simulation.force('collide', forceCollide);
-						};
-					})
-					.on('end', () => {
-						focusedNode = null;
-						simulation.alphaTarget(0);
-					})
-					.on('interrupt', () => {
-						simulation.alphaTarget(0);
-					});
-
-				// hide all circle-overlay
-				d3.selectAll('.circle-overlay').classed('hidden', true);
-				d3.selectAll('.node-icon').classed('node-icon--faded', false);
-			}
+		node.on('mouseout', function(currentNode) {
+			// rmv ring
+			const circle = d3.select(this);
+			circle
+				.attr('stroke','none');
 		});
 
 		function ticked() {
