d44b200327d5625b9b6cb1bdc8636241472099fa
jpecht <me.jefferson@gmail.com>
2017-09-05 19:35:33 -0600

    updated costs definitions

--- js/components/definitions.js
+++ js/components/definitions.js
@@ -1,8 +1,8 @@
 (() => {
 	App.definitions = {
 		totalCost: 'The <b>total cost</b>, depending on the duration selected, is the sum of the startup cost, capital cost, and recurring costs.',
-		startupCost: 'The <b>startup cost</b> refers to non-recurring costs.',
-		capitalCost: 'The <b>capital cost</b> refers to salary related capital expenditures.',
-		recurringCost: 'The <b>recurring cost</b> refers to annual operating costs.',
+		startupCost: '<b>Startup costs</b> are one-time expenses incurred in the first year, excluding construction and durable equipment costs.',
+		capitalCost: '<b>Capital costs</b> are non-recurring costs incurred in the first year for construction and durable equipment.',
+		recurringCost: '<b>Recurring costs</b> are annual operating expenses, including salaries.',
 	};
 })();
