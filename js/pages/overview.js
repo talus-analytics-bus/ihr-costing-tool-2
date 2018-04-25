343eb19907f3e000037e192a34dbd9dd5d87710f
seaneff-ta <seaneff@talusanalytics.com>
2018-04-25 09:11:18 -0400

    remove hover-over that references qlik tool

--- js/pages/overview.js
+++ js/pages/overview.js
@@ -2,12 +2,6 @@
 	App.initOverview = () => {
 		$('.next-button').click(() => { hasher.setHash('scores/'); });
 
-		// jee tool tooltip
-		$('.jee-tool-img').tooltipster({
-			trigger: 'hover',
-			content: 'The <b>JEE Technology Tool</b> is a separate, open-access tool developed by the Global Health Security Agenda Private Sector Roundtable Technology and Analytics Working Group and powered by Qlik Technologies.',
-		});
-
 		/* ------------------ Uploading Session File --------------- */
 		// clicking "select file from previous session" triggers file selection
 		$('.btn-prior-session').on('click', () => { 
