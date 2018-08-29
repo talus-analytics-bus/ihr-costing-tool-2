cf530103db2f31ccf3366bc29a8d1459f47a1ace
mevanmaele <mvanmaele@talusanalytics.com>
2018-08-29 11:22:49 -0400

    Demo

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const App = {};
 (() => {
 	App.lang = 'en';
 	App.choseLang = false;
-	App.demoMode = false;
+	App.demoMode = true;
 	App.cookieDomain = "";
 
 	App.scoreLabels = App.lang === 'fr' ? {
